a346c22aeccdf0b5007b2ff938b0a34572676050
Alejandro Steinmetz <asteinmetz78@gmail.com>
2017-08-29 19:10:36 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-app toolbar>
+     <v-navigation-drawer
+      temporary
+      v-model="sideNav" 
+      :mini-variant.sync="mini" 
+      light 
+      overflow
+      absolute
+    >
+      <v-list>
+        <v-list-tile
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.link">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+     <!-- <v-toolbar dark class="primary">
+      <v-toolbar-side-icon 
+        @click.stop="sideNav = !sideNav"
+        class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title><router-link to="/" tag="span" style="cursor: pointer">DevMeetup</router-link></v-toolbar-title>
+      <v-toolbar-items class="hidden-sm-and-down">
+       <v-btn
+          flat
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.link">
+          <v-icon left dark>{{ item.icon }}</v-icon>
+          {{ item.title }}
+        </v-btn>
+    </v-toolbar-items>
+    </v-toolbar> -->
+    <v-toolbar dark class="primary">
+      <v-toolbar-side-icon 
+        @click.stop="sideNav = !sideNav"
+        class="hidden-md-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title><router-link to="/" tag="span" style="cursor: pointer">DevMeetup</router-link></v-toolbar-title>
+      <v-spacer></v-spacer>      
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn 
+          flat 
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.link">
+          <v-icon left dark>{{ item.icon }}</v-icon>
+          {{item.title}}
+        </v-btn>
+        
+      </v-toolbar-items>
+  </v-toolbar>
+    <main>
+      <v-container fluid>        
+        <router-view></router-view>
+      </v-container>
+    </main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        sideNav: false,
+        menuItems: [
+          { icon: 'person', title: 'Profile', link: '/profile' },
+          { icon: 'room', title: 'Organize Meetup', link: '/meetup/new' },
+          { icon: 'supervisor_account', title: 'View Meetups', link: '/meetups' }
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
---
+++ src/components/Meetup/Meetups.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>    
+    <p>View Meetups</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return { }
+    }
+  }
+</script>
