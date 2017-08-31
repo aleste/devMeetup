eac9e1e22001d5ad0222637c92b51c085e0142ee
Alejandro Steinmetz <asteinmetz78@gmail.com>
2017-08-30 21:45:58 -0300

    Adds signin & signup functions

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from './App'
+import * as firebase from 'firebase'
 import router from './router'
 import { store } from './store'
 import DateFilter from './filters/date'
@@ -15,5 +16,14 @@ new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyA2hlKUJchQqT4vzNaOqSf9yPtBZ48Bdpk',
+      authDomain: 'devmeetup-e534b.firebaseapp.com',
+      databaseURL: 'https://devmeetup-e534b.firebaseio.com',
+      projectId: 'devmeetup-e534b',
+      storageBucket: 'devmeetup-e534b.appspot.com'
+    })
+  }
 })
